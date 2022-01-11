af1c0c89427c61996cceb957f84ff6e36f517981
nect2r <nect2r@kakao.com>
2022-01-11 23:01:59 +0900

    @Query를 통해서 바로 SQL을 사용할 수 있음
    NamedQuery 의 장점을 그대로 쓸 수 있어서 실무에서도 많이사용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -79,4 +79,16 @@ class MemberRepositoryTest {
         Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
     }
 
+    @Test
+    public void testQuery () throws Exception {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA",10);
+
+        Assertions.assertThat(result.get(0)).isEqualTo(m1);
+    }
+
 }
